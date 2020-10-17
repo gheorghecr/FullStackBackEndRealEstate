822d406888bb7a26edd5bec4b35345a56be57d67
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-10-17 14:48:11 +0000

    Added API Documentation for Users API's

---
+++ docs.js
@@ -0,0 +1,13 @@
+const Koa = require('koa');
+const serve = require('koa-static');
+const mount = require('koa-mount');
+const app = new Koa();
+
+app.use(mount('/', serve('./docs/jsdocs')))  // serve JSDocs
+app.use(mount('/openapi', serve('./docs/openapi')))  // serve OpenAPI
+app.use(mount('/schemas', serve('./schemas')))  // serve schemas
+
+let port = process.env.PORT || 3030;
+
+app.listen(port);
+console.log(`OpenAPI server running on port ${port}`);
