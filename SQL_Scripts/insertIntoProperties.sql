d4adcb814dcdd5f18a2acdc2dc1c999595085c03
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-10-18 21:13:02 +0000

    Added sql scripts for properties & status tables

---
+++ SQL_Scripts/insertIntoProperties.sql
@@ -0,0 +1,8 @@
+INSERT INTO properties
+  ( price, title, description, status, sellerID)
+VALUES
+  (100000, 'Prop 1', 'Descrip 1', 'For Sale', 20 ),
+  (200000, 'Prop 2', 'Descrip 2', 'For Sale', 21 ), 
+  (300000, 'Prop 3', 'Descrip 3', 'Sold', 20 ), 
+  (400000, 'Prop 4', 'Descrip 4', 'Under Offer', 20 ); 
+  
---
+++ SQL_Scripts/insertIntoStatus.sql
@@ -0,0 +1,7 @@
+INSERT INTO status
+  ( name, description)
+VALUES
+  ('For Sale', 'When property is available for sale' ),
+  ('Under Offer', 'When property is under offer' ),
+  ('Sold', 'Buyer Has Been Found' );
+  
