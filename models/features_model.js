d7884bd52dd37c452c281c1b0fa542ba4c83d778
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-10-23 14:32:12 +0000

    Added route to get the features for a giver property. Including documentation

--- models/features_model.js
+++ models/features_model.js
@@ -14,3 +14,15 @@ exports.getAllFeatures = async function getAllFeatures() {
     const data = await db.run_query(query);
     return data;
 };
+
+/**
+ * SQL Query function to get the features for a given property.
+ * @param {integer} id - property ID.
+ * @returns {object} data - The response object containing all features for a given property.
+ */
+exports.getFeaturesForProperty = async function getFeaturesForProperty(id) {
+    let query = 'SELECT f.ID, f.name FROM propertyFeatures as pf INNER JOIN features AS f';
+    query += ' ON pf.featureID = f.ID WHERE pf.propertyID = ?;';
+    const result = await db.run_query(query, id);
+    return result;
+};
--- routes/features_routes.js
+++ routes/features_routes.js
@@ -38,9 +38,26 @@ async function getAllFeatures(cnx) {
     }
 }
 
+/**
+ * Function that gets the list of features for a given property.
+ * @param {object} cnx - The request object.
+ * @returns {object} cnx - The response object.
+ */
+async function getAllFeaturesForProperty(cnx) {
+    const propID = cnx.params.id;
+
+    const result = await model.getFeaturesForProperty(propID);
+    if (result.length) {
+        cnx.status = 200;
+        cnx.body = result;
+    } else {
+        cnx.status = 404;
+    }
+}
+
 // Gets
 router.get('/', getAllFeatures);
-//router.get('/:id([0-9]{1,})', getAllCategoriesForProperty);
+router.get('/:id([0-9]{1,})', getAllFeaturesForProperty);
 
 // Delete
 //router.del('/:id([0-9]{1,})', auth, deleteById);
