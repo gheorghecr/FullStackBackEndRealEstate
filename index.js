fe04e7f13cef7bfc5b135a3aa480d85f5b3f720a
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-08 20:36:31 +0000

    Added the images router to the index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 */
 
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
@@ -14,12 +15,15 @@ const users = require('./routes/users_routes.js');
 const categories = require('./routes/categories_routes.js');
 const features = require('./routes/features_routes.js');
 const messages = require('./routes/messages_routes.js');
+const images = require('./routes/images_routes.js');
 
 app.use(properties.routes());
 app.use(users.routes());
 app.use(categories.routes());
 app.use(features.routes());
 app.use(messages.routes());
+app.use(images.routes());
+app.use(bodyParser({ multipart: true }));
 
 // Run the app as a process on a given port
 const port = process.env.PORT || 3000;
