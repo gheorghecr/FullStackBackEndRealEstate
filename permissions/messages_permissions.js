fc2d3d2c22e7bb2c5e674cccc1e87021d3766722
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-25 21:46:32 +0000

    Changed permisson on messages. Now only owners(agents) can delete/update the messages

--- permissions/messages_permissions.js
+++ permissions/messages_permissions.js
@@ -16,11 +16,11 @@ ac.grant('user').condition({ Fn: 'EQUALS', args: { 'requester': '$.owner' } }).e
 
 // Admin permissions
 ac.grant('admin').execute('read').on('messages');
-ac.grant('admin').condition({ Fn: 'NOT_EQUALS', args: { 'requester': '$.owner' } }).execute('delete').on('messages');
 ac.grant('admin').condition({ Fn: 'EQUALS', args: { 'requester': '$.owner' } }).execute('delete').on('messages');
-ac.grant('admin').execute('update').on('messages');
+ac.grant('admin').condition({ Fn: 'EQUALS', args: { 'requester': '$.owner' } }).execute('delete').on('messages');
+ac.grant('admin').condition({ Fn: 'EQUALS', args: { 'requester': '$.owner' } }).execute('update').on('messages');
 
 exports.readAllForConversation = (requester) => ac.can(requester.role).execute('read').sync().on('messages');
-exports.toggleArchived = (requester) => ac.can(requester.role).execute('update').sync().on('messages');
-exports.deleteByID = (requester, data) => ac.can(requester.role).context({ requester: requester.userID, owner: data.fromID }).execute('delete').sync().on('messages');
-exports.readByID = (requester, data) => ac.can(requester.role).context({ requester: requester.userID, owner: data.fromID }).execute('read').sync().on('messages');
+exports.toggleArchived = (requester, data) => ac.can(requester.role).context({ requester: requester.userID, owner: data.agentID }).execute('update').sync().on('messages');
+exports.deleteByID = (requester, data) => ac.can(requester.role).context({ requester: requester.userID, owner: data.agentID }).execute('delete').sync().on('messages');
+exports.readByID = (requester, data) => ac.can(requester.role).context({ requester: requester.userID, owner: data.agentID }).execute('read').sync().on('messages');
