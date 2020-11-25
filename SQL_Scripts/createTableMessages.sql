b2fb1233f28eb10427a78693dd4c5f4243f5de3a
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-25 17:28:16 +0000

    Updated the sql script for the table

--- SQL_Scripts/createTableMessages.sql
+++ SQL_Scripts/createTableMessages.sql
@@ -1,13 +1,12 @@
 CREATE TABLE messages (
     messageID INT NOT NULL AUTO_INCREMENT, 
-    fromID INT  NOT NULL,
-    toID INT NOT NULL,
+    fromEmail VARCHAR(50)  NOT NULL,
+    fromName VARCHAR(50)  NOT NULL,
+    agentID INT NOT NULL,
     propertyID INT,
-    conversationID INT,
     messageText TEXT NOT NULL, 
     archived BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (messageID),
-    FOREIGN KEY (fromID) REFERENCES users (userID) ON DELETE CASCADE,
     FOREIGN KEY (propertyID) REFERENCES properties (prop_ID) ON DELETE CASCADE,
-    FOREIGN KEY (toID) REFERENCES users (userID) ON DELETE CASCADE
+    FOREIGN KEY (agentID) REFERENCES users (userID) ON DELETE CASCADE
 );
