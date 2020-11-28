62cdb10ccb32238613a6d59bba84965f63f7dbec
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-28 19:04:28 +0000

    Updading messages sql script. Removing NOT NULL from the fromNumber

--- SQL_Scripts/createTableMessages.sql
+++ SQL_Scripts/createTableMessages.sql
@@ -2,7 +2,7 @@ CREATE TABLE messages (
     messageID INT NOT NULL AUTO_INCREMENT, 
     fromEmail VARCHAR(50)  NOT NULL,
     fromName VARCHAR(50)  NOT NULL,
-    fromNumber VARCHAR(50)  NOT NULL,
+    fromNumber VARCHAR(50),
     agentID INT NOT NULL,
     propertyID INT,
     messageText TEXT NOT NULL, 
