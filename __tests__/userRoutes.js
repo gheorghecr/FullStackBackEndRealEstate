567348de617c7a0b341d51356deb77dbf5458577
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-29 21:11:23 +0000

    Adding test for login

--- __tests__/userRoutes.js
+++ __tests__/userRoutes.js
@@ -3,6 +3,17 @@ const request = require('supertest');
 
 const app = require('../app');
 
+beforeAll(async (done) => {
+    await request(app.callback())
+        .post('/api/users')
+        .field('firstName', 'Gheorghe')
+        .field('lastName', 'craciun')
+        .field('password', 'password')
+        .field('username', 'gheorghe')
+        .field('email', 'unique_email2@example.com');
+    done();
+});
+
 describe('Post new user', () => {
     it('should create a new user', async () => {
         const res = await request(app.callback())
@@ -21,8 +32,27 @@ describe('Login', () => {
     it('Should login ', async () => {
         const res = await request(app.callback())
             .post('/api/users/login')
-            .auth('testAD', 'testPassword');
+            .auth('gheorghe', 'password');
         expect(res.statusCode).toEqual(201);
-        //expect(res.body).toHaveProperty('created', true);
+    });
+});
+
+describe('Login with wrong username', () => {
+    it('Should fail login ', async () => {
+        const res = await request(app.callback())
+            .post('/api/users/login')
+            .auth('wrongID', 'password');
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toHaveProperty('errorMessage', 'Username does not exist');
+    });
+});
+
+describe('Login with wrong password', () => {
+    it('Should fail login ', async () => {
+        const res = await request(app.callback())
+            .post('/api/users/login')
+            .auth('gheorghe', 'passee');
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toHaveProperty('errorMessage', 'Incorrect password');
     });
 });
