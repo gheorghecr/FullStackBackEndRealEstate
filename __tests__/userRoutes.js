78b5d91770879cfc2a0ef38740f59e9e561bd935
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-11-29 22:01:14 +0000

    Adding unit test for deleteing acount, get the list of users, and login in with wrong details

--- __tests__/userRoutes.js
+++ __tests__/userRoutes.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 /* eslint-disable no-undef */
 const request = require('supertest');
 
@@ -10,7 +11,8 @@ beforeAll(async (done) => {
         .field('lastName', 'craciun')
         .field('password', 'password')
         .field('username', 'gheorghe')
-        .field('email', 'unique_email2@example.com');
+        .field('email', 'unique_email2@example.com')
+        .field('sign_up_code', 'we_sell_houses_agent');
     done();
 });
 
@@ -54,5 +56,43 @@ describe('Login with wrong password', () => {
             .auth('gheorghe', 'passee');
         expect(res.statusCode).toEqual(401);
         expect(res.body).toHaveProperty('errorMessage', 'Incorrect password');
+    });
+});
+
+describe('Get a list of all users', () => {
+    it('should return a list of users', async () => {
+        const res = await request(app.callback())
+            .get('/api/users')
+            .auth('gheorghe', 'password');
+        expect(res.statusCode).toEqual(200);
+    });
+});
+
+describe('Get a list of all users', () => {
+    it('should fail as user is not registered', async () => {
+        const res = await request(app.callback())
+            .get('/api/users')
+            .auth('giko1997', 'password');
+        expect(res.statusCode).toEqual(401);
+    });
+});
+
+describe('Register and then delete account', () => {
+    it('should register an account and then delete it', async () => {
+        const res = await request(app.callback())
+            .post('/api/users')
+            .field('firstName', 'unique1111')
+            .field('lastName', 'craciun')
+            .field('password', 'password')
+            .field('username', 'uni11111')
+            .field('email', 'unique_email1111@example.com');
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toHaveProperty('id');
+
+        const res2 = await request(app.callback())
+            .delete(`/api/users/${res.body.id}`)
+            .auth('gheorghe', 'password');
+        expect(res2.statusCode).toEqual(200);
+        expect(res2.body).toHaveProperty('deleted', true);
     });
 });
