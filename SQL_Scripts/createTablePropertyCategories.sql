0ef9b25d33990baabf6ade3ac6b5c1777ebaa412
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-12-05 12:14:15 +0000

    Changing the property categories table

--- SQL_Scripts/createTablePropertyCategories.sql
+++ SQL_Scripts/createTablePropertyCategories.sql
@@ -1,7 +1,8 @@
 CREATE TABLE propertyCategories (
+  propertyCategoryID INT NOT NULL AUTO_INCREMENT,
   propertyID INT NOT NULL,
   categoryID INT NOT NULL,
   FOREIGN KEY (propertyID) REFERENCES properties (prop_ID) ON DELETE CASCADE,
   FOREIGN KEY (categoryID) REFERENCES categories (ID) ON DELETE CASCADE,
-  PRIMARY KEY (propertyID, categoryID)
+  PRIMARY KEY (propertyCategoryID)
 );
