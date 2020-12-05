bb56026b369177f0458ea3f57dd5853721886464
Gheorghe Craciun <craciun2@coventry.ac.uk>
2020-12-05 11:19:03 +0000

    Changed the propertyFeatures table

--- SQL_Scripts/createTablePropertyFeatures.sql
+++ SQL_Scripts/createTablePropertyFeatures.sql
@@ -1,7 +1,8 @@
 CREATE TABLE propertyFeatures (
+  propertyFeatureID INT NOT NULL AUTO_INCREMENT,
   propertyID INT NOT NULL,
   featureID INT NOT NULL,
   FOREIGN KEY (propertyID) REFERENCES properties (prop_ID) ON DELETE CASCADE,
   FOREIGN KEY (featureID) REFERENCES features (ID) ON DELETE CASCADE,
-  PRIMARY KEY (propertyID, featureID)
+  PRIMARY KEY (propertyFeatureID)
 );
